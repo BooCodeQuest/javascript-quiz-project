993a7e780cd301b168131b1b4e65076adb81dc83
BooCodeQuest <nightopshere@skiff.com>
2024-01-15 17:02:27 +0100

    update Iteration 1 & 2

--- src/question.js
+++ src/question.js
@@ -3,5 +3,36 @@ class Question {
     //
     // 1. constructor (text, choices, answer, difficulty)
 
+   
+    
+        constructor(text, choices, answer, difficulty) {
+          this.text = text;                   // Assigns the 1st argument (string) to the text property
+          this.choices = choices;             // Assigns the 2nd argument (array of strings) to the choices property
+          this.answer = answer;               // Assigns the 3rd argument (string) to the answer property
+          this.difficulty = difficulty;       // Assigns the 4th argument (number) to the difficulty property
+        }
+
     // 2. shuffleChoices()
+
+    shuffleChoices() {
+      // Create a copy of the choices array
+      let newChoices = [...this.choices];
+
+      // Swap the last element with a random one, ensuring at least one swap
+      let last = newChoices.length - 1;
+      let j = Math.floor(Math.random() * (last + 1));
+      [newChoices[last], newChoices[j]] = [newChoices[j], newChoices[last]];
+
+      // Continue with the rest of the shuffle
+      for (let i = last - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          [newChoices[i], newChoices[j]] = [newChoices[j], newChoices[i]];
+      }
+
+      // Assign the shuffled array back to this.choices
+      this.choices = newChoices;
+
+      // Return the shuffled choices
+      return this.choices;
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,64 @@
-class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    // 1. constructor (questions, timeLimit, timeRemaining
+    class Quiz {
+        constructor(questions, timeLimit, timeRemaining) {
+          this.questions = questions; // Array of Question objects
+          this.timeLimit = timeLimit; // Total time limit for the quiz
+          this.timeRemaining = timeRemaining; // Remaining time for the quiz
+          this.correctAnswers = 0; // Count of correct answers
+          this.currentQuestionIndex = 0; // Index of the current question
 
+        }
     // 2. getQuestion()
-    
+        getQuestion() {
+          // Returns the current question
+          return this.questions[this.currentQuestionIndex];
+        }
     // 3. moveToNextQuestion()
+        moveToNextQuestion() {
+          // Increments the index to move to the next question
+          this.currentQuestionIndex++;
+        }
 
-    // 4. shuffleQuestions()
+    // 4. shuffleQuestions
+        shuffleQuestions() {
+          // Create a copy of the questions array
+          let newChoices = [...this.questions];
+  
+          // Fisher-Yates shuffle algorithm
+          for (let i = newChoices.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [newChoices[i], newChoices[j]] = [newChoices[j], newChoices[i]];
+          }
+  
+          // Assign the shuffled array back to this.questions
+          this.questions = newChoices;
+  
+          // Return the shuffled questions
+          return this.questions;
+      }
 
-    // 5. checkAnswer(answer)
 
+
+
+
+     // 5. checkAnswer(answer)
+        checkAnswer(answer) {
+          // Checks if the provided answer matches the answer of the current question
+          const currentQuestion = this.questions[this.currentQuestionIndex];
+          if (currentQuestion && answer === currentQuestion.answer) {
+            this.correctAnswers++;
+          }
+        }
     // 6. hasEnded()
-}
+        hasEnded() {
+          // Check if the quiz has reached the end
+          return this.currentQuestionIndex >= this.questions.length;
+        }
+      }
+
+
+
+
+
+
